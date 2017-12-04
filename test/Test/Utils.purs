1164fe5a0f8110bf00079564da41f380817306f2
Maciej Bielecki <zyla@prati.pl>
2017-12-04 11:19:41 +0100

    Add Test.Utils.shouldHaveInferredType

--- test/Test/Utils.purs
+++ test/Test/Utils.purs
@@ -11,6 +11,7 @@ import Data.IORef (IORef, modifyIORef, readIORef, writeIORef)
 import Specular.Dom.Browser (Node)
 import Specular.Dom.Node.Class (EventType)
 import Test.Spec.Assertions (shouldEqual)
+import Type.Prelude (Proxy)
 
 append :: forall a. IORef (Array a) -> a -> IOSync Unit
 append ref value = modifyIORef ref (\a -> snoc a value)
@@ -32,3 +33,19 @@ ioSync = liftEff <<< unsafeCoerceEff <<< runIOSync
 -- | Dispatch an Event with the given type and no additional information
 -- | on the node.
 foreign import dispatchTrivialEvent :: Node -> EventType -> IOSync Unit
+
+class ShouldHaveInferredType actual expected where
+  -- | Assert that the given value has inferred type @expected@.
+  -- | If the assertion turns out to be false, the call will not compile.
+  -- |
+  -- | For example, the following should not compile:
+  -- |
+  -- | ```
+  -- | let _ = mempty `shouldHaveInferredType` (Proxy :: Proxy (Array Int))
+  -- | ```
+  -- |
+  -- | While `mempty :: Array Int` typechecks, `mempty` itself has no concrete type.
+  shouldHaveInferredType :: actual -> Proxy expected -> Unit
+
+instance shouldHaveInferredTypeInstance :: ShouldHaveInferredType a a where
+  shouldHaveInferredType _ _ = unit
