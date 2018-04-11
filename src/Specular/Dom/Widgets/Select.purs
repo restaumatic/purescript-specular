69e43a5f88a4f5ee0722186e7b0c0835b02178d5
Maciej Bielecki <zyla@prati.pl>
2018-04-11 17:39:58 +0200

    Add selectView

--- src/Specular/Dom/Widgets/Select.purs
+++ src/Specular/Dom/Widgets/Select.purs
@@ -1,12 +1,16 @@
 module Specular.Dom.Widgets.Select
   ( SelectConfig
   , selectInput
+
+  , SelectViewConfig
+  , selectView
   ) where
 
 import Prelude
 
 import Control.Monad.IOSync.Class (liftIOSync)
 import Data.Array as Array
+import Data.Foldable (for_)
 import Data.FoldableWithIndex (traverseWithIndex_)
 import Data.Int as Int
 import Data.Tuple (Tuple(..))
@@ -15,8 +19,9 @@ import Specular.Dom.Builder.Class (domEventWithSample, elAttr, elDynAttr', text)
 import Specular.Dom.Node.Class (Attrs, (:=))
 import Specular.Dom.Widget (class MonadWidget)
 import Specular.Dom.Widgets.Input (getTextInputValue, setTextInputValue)
-import Specular.FRP (Dynamic, WeakDynamic, holdDyn)
+import Specular.FRP (Dynamic, Event, WeakDynamic, holdDyn)
 import Specular.FRP.Base (filterMapEvent)
+import Specular.FRP.WeakDynamic (subscribeWeakDyn_)
 
 type SelectConfig a =
   { options :: Array a              -- ^ Possible selections
@@ -42,3 +47,28 @@ selectInput config = do
     initialValue = unsafePartial (Array.unsafeIndex config.options config.initialValueIndex)
 
   holdDyn initialValue valueChanged
+
+type SelectViewConfig a =
+  { options :: Array a
+  , display :: a -> String
+  , attributes :: WeakDynamic Attrs
+  }
+
+-- | A <select> element with static list of options.
+selectView
+  :: forall m a
+   . MonadWidget m
+  => Eq a
+  => SelectViewConfig a
+  -> WeakDynamic a -- ^ selected option
+  -> m (Event a)   -- ^ event: user changed selection
+selectView config valueD = do
+  let toOption index value = elAttr "option" ("value" := show index) $ text $ config.display value
+  Tuple element _ <- elDynAttr' "select" config.attributes $ traverseWithIndex_ toOption config.options
+
+  flip subscribeWeakDyn_ valueD $ \value ->
+    for_ (Array.findIndex (eq value) config.options) $ \index ->
+      liftIOSync $ setTextInputValue element $ show index
+
+  domChanged <- domEventWithSample (\_ -> getTextInputValue element) "change" element
+  pure $ filterMapEvent (Int.fromString >=> Array.index config.options) domChanged
