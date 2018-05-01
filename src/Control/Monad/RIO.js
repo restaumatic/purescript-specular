b14c6ad72906c3797141e759883472f60df6d290
Maciej Bielecki <zyla@prati.pl>
2018-05-01 10:32:00 +0200

    Change internals to use RIO

--- src/Control/Monad/RIO.js
+++ src/Control/Monad/RIO.js
@@ -1,6 +1,6 @@
 // pureImpl  :: forall r a.   a                                -> RIO r a
 exports.pureImpl = function(x) {
-  return function(_) {
+  return function RIO_pure_eff(_) {
     return x;
   };
 };
@@ -8,7 +8,7 @@ exports.pureImpl = function(x) {
 // mapImpl   :: forall r a b. (a -> b)       -> RIO r a        -> RIO r b
 exports.mapImpl = function(f) {
   return function(io_x) {
-    return function(env) {
+    return function RIO_map_eff(env) {
       return f(io_x(env));
     };
   };
@@ -17,7 +17,7 @@ exports.mapImpl = function(f) {
 // applyImpl :: forall r a b. RIO r (a -> b) -> RIO r a        -> RIO r b
 exports.applyImpl = function(io_f) {
   return function(io_x) {
-    return function(env) {
+    return function RIO_apply_eff(env) {
       var f = io_f(env);
       return f(io_x(env));
     };
@@ -27,21 +27,21 @@ exports.applyImpl = function(io_f) {
 // bindImpl  :: forall r a b. RIO r a        -> (a -> RIO r b) -> RIO r b
 exports.bindImpl = function(io_x) {
   return function(k) {
-    return function(env) {
+    return function RIO_bind_eff(env) {
       return k(io_x(env))(env);
     };
   };
 };
 
 // askImpl   :: forall r. RIO r r
-exports.askImpl = function(env) {
+exports.askImpl = function RIO_ask_eff(env) {
   return env;
 };
 
 // runRIO :: forall r a. r -> RIO r a -> IOSync a
 exports.runRIO = function(env) {
   return function(io) {
-    return function() {
+    return function runRIO_eff() {
       return io(env);
     };
   };
@@ -49,7 +49,7 @@ exports.runRIO = function(env) {
 
 // rio :: forall r a. (r -> IOSync a) -> RIO r a
 exports.rio = function(f) {
-  return function(env) {
+  return function RIO_rio_eff(env) {
     return f(env)();
   };
 };
@@ -57,7 +57,7 @@ exports.rio = function(f) {
 // local :: forall r e a. (e -> r) -> RIO r a -> RIO e a
 exports.local = function(f) {
   return function(io) {
-    return function(env) {
+    return function RIO_local_eff(env) {
       return io(f(env));
     };
   };
--- src/Specular/FRP/Base.js
+++ src/Specular/FRP/Base.js
@@ -1,13 +1,15 @@
-// Frame a ~ DelayedEffects -> Time -> IOSync Unit
 // sequenceFrame_ :: Array (Frame Unit) -> Frame Unit
 exports.sequenceFrame_ = function(xs) {
-  return function(r1) {
-    return function(r2) {
-      return function sequenceFrame_eff() {
-        for(var i = 0; i < xs.length; i++) {
-          xs[i](r1)(r2)();
-        }
-      };
-    };
+  return function sequenceFrame_eff(env) {
+    for(var i = 0; i < xs.length; i++) {
+      xs[i](env);
+    }
+  };
+};
+
+// unsafeMkPull :: forall a. (Time -> IOSync a) -> Pull a
+exports.unsafeMkPull = function(f) {
+  return function(env) {
+    return f(env)();
   };
 };
