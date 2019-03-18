2d0b1bf6f57c8fa8835384b2dafd56a6204accbd
Maciej Bielecki <maciej.bielecki@skubacz.pl>
2019-03-18 07:25:47 +0100

    Use concrete Node type instead of the DOM type class (#37)
    Motivation:
    - To make code simpler
    - To remove some indirections, in hope of improving performance. Note
      that this patch doesn't have any performance impact on current
      benchmarks.
    
    Another implementation of this abstraction didn't arise so far. One of
    the intended use cases was some kind of server-side rendering, but it's
    not obvious whether this abstraction would help.

--- src/Specular/Dom/Builder.purs
+++ src/Specular/Dom/Builder.purs
@@ -15,8 +15,9 @@ import Data.Tuple (Tuple(Tuple))
 import Effect (Effect)
 import Effect.Class (class MonadEffect, liftEffect)
 import Foreign.Object as SM
+import Specular.Dom.Browser (Node)
 import Specular.Dom.Builder.Class (class MonadDetach, class MonadDomBuilder)
-import Specular.Dom.Node.Class (class DOM, appendChild, appendRawHtml, createDocumentFragment, createElementNS, createTextNode, insertBefore, moveAllBetweenInclusive, parentNode, removeAllBetween, removeAttributes, setAttributes, setText)
+import Specular.Dom.Node.Class (appendChild, appendRawHtml, createDocumentFragment, createElementNS, createTextNode, insertBefore, moveAllBetweenInclusive, parentNode, removeAllBetween, removeAttributes, setAttributes, setText)
 import Specular.FRP.WeakDynamic (subscribeWeakDyn_)
 import Specular.Internal.Effect (DelayedEffects, emptyDelayed, modifyRef, newRef, pushDelayed, readRef, sequenceEffects, unsafeFreezeDelayed, writeRef)
 import Specular.Internal.RIO (RIO, rio, runRIO)
@@ -59,7 +60,7 @@ getEnv = Builder ask
 setParent :: forall node. node -> BuilderEnv node -> BuilderEnv node
 setParent parent env = env { parent = parent }
 
-instance monadReplaceBuilder :: DOM node => MonadReplace (Builder node) where
+instance monadReplaceBuilder :: MonadReplace (Builder Node) where
 
   newSlot = do
     env <- getEnv
@@ -71,7 +72,7 @@ instance monadReplaceBuilder :: DOM node => MonadReplace (Builder node) where
     cleanupRef <- liftEffect $ newRef (mempty :: Effect Unit)
 
     let
-      replace :: forall a. Builder node a -> Effect a
+      replace :: forall a. Builder Node a -> Effect a
       replace inner = do
         fragment <- createDocumentFragment
         Tuple result cleanup <- runBuilder fragment inner
@@ -100,7 +101,7 @@ instance monadReplaceBuilder :: DOM node => MonadReplace (Builder node) where
       destroy = do
         join $ readRef cleanupRef
 
-      append :: Effect (Slot (Builder node))
+      append :: Effect (Slot (Builder Node))
       append = do
         fragment <- createDocumentFragment
         Tuple slot cleanup <- runBuilder fragment newSlot
@@ -120,7 +121,7 @@ instance monadReplaceBuilder :: DOM node => MonadReplace (Builder node) where
 
     pure $ Slot { replace, destroy, append }
 
-instance monadDomBuilderBuilder :: DOM node => MonadDomBuilder node (Builder node) where
+instance monadDomBuilderBuilder :: MonadDomBuilder (Builder Node) where
 
   text str = mkBuilder \env -> do
     node <- createTextNode str
@@ -165,7 +166,7 @@ instance monadDomBuilderBuilder :: DOM node => MonadDomBuilder node (Builder nod
     liftEffect $ appendChild node env.parent
     pure result
 
-instance monadDetachBuilder :: DOM node => MonadDetach (Builder node) where
+instance monadDetachBuilder :: MonadDetach (Builder Node) where
   detach inner = do
     fragment <- liftEffect createDocumentFragment
 
