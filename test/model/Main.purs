ccbf9ee55c7b188f94873e07a4dfb6336b565be5
Maciej Bielecki <zyla@prati.pl>
2019-11-03 10:29:13 +0100

    Pretty-printing

--- test/model/Main.purs
+++ test/model/Main.purs
@@ -3,6 +3,7 @@ module Test.ModelMain where
 import Prelude
 
 import Control.Lazy (defer)
+import Data.Array (fromFoldable) as Array
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
 import Data.List (List(..))
@@ -18,47 +19,17 @@ main = do
   b <- checkM do
     forAll (genProgram 0) \program -> do
       resultM do
-        Console.log $ show program
+        Console.log "--------------------------------------------------------------------------------"
+        Console.log $ ppProgram program
         pure $ Success -- Failure $ pure $ show program
   if not b then unsafeCrashWith "Failure" else pure unit
 
 type Value = String
+type Fn = String -- uninterpreted function
 type Time = Int
+type VarId = Int
 
 type SemDynamic a = List (Tuple Time a)
-
-data Program
-  = NewDynamic (SemDynamic Value) Program
-  | Return Expr_Dynamic
-
-derive instance genericProgram :: Generic Program _
-instance showProgram :: Show Program where show x = genericShow x
-
-data Expr_Dynamic
-  = Var Int
-  | Pure Expr_Value
-  | Map Expr_Value Expr_Dynamic
-  | Map2 Expr_Value Expr_Dynamic Expr_Dynamic
-
-derive instance genericExpr_Dynamic :: Generic Expr_Dynamic _
-instance showExpr_Dynamic :: Show Expr_Dynamic where show x = genericShow x
-
-data Expr_Value
-  = ConstantValue Value
-  | ApplyValue Expr_Value Expr_Value
-
-derive instance genericExpr_Value :: Generic Expr_Value _
-instance showExpr_Value :: Show Expr_Value where show x = genericShow x
-
-type Env = Int -- number of variables available
-
-emptyEnv :: Env
-emptyEnv = 0
-
-genProgram :: Env -> Gen Program
-genProgram env = oneOfRec
-  [ Return <$> genExprDynamic env ]
-  [ defer \_ -> NewDynamic <$> genSemDynamic <*> genProgram (env + 1) ]
 
 genSemDynamic :: Gen (SemDynamic Value)
 genSemDynamic = go 0
@@ -72,17 +43,70 @@ genSemDynamic = go 0
         ]
       pure (Cons (Tuple t value) next)
 
+data Program
+  = NewDynamic Int (SemDynamic Value) Program
+  | Return Expr_Dynamic
+
+derive instance genericProgram :: Generic Program _
+instance showProgram :: Show Program where show x = genericShow x
+
+genProgram :: Env -> Gen Program
+genProgram env = oneOfRec
+  [ Return <$> genExprDynamic env ]
+  [ defer \_ -> NewDynamic env <$> genSemDynamic <*> genProgram (env + 1) ]
+
+ppProgram :: Program -> String
+ppProgram =
+  case _ of
+    NewDynamic binder sem next ->
+      "v" <> show binder <> " <- newDynamic -- " <> show (Array.fromFoldable sem) <> "\n" <>
+      ppProgram next
+
+    Return expr ->
+      "pure (" <> ppExprDynamic expr <> ")"
+
+{-
+data Expr_Value
+  = ConstantValue Value
+  | ApplyValue Expr_Value Expr_Value
+
+derive instance genericExpr_Value :: Generic Expr_Value _
+instance showExpr_Value :: Show Expr_Value where show x = genericShow x
+-}
+
+type Env = Int -- number of variables available
+
+emptyEnv :: Env
+emptyEnv = 0
+
 genValue :: Gen Value
-genValue = oneOf $ map pure [ "x", "y", "z" ]
+genValue = oneOf $ map pure [ "a", "b", "c", "d", "x", "y", "z" ]
+
+genFn :: Gen Fn
+genFn = oneOf $ map pure [ "f", "g", "h" ]
+
+data Expr_Dynamic
+  = Var Int
+  | Pure Value
+  | Map Fn Expr_Dynamic
+  | Map2 Fn Expr_Dynamic Expr_Dynamic
+
+derive instance genericExpr_Dynamic :: Generic Expr_Dynamic _
+instance showExpr_Dynamic :: Show Expr_Dynamic where show x = genericShow x
 
 genExprDynamic :: Env -> Gen Expr_Dynamic
 genExprDynamic env = oneOfRec
   ((if env > 0 then [ Var <$> chooseInt 0 (env-1) ] else []) <>
-  [ Pure <$> genExprValue
+  [ Pure <$> genValue
   ])
-  []
+  [ defer \_ -> Map <$> genFn <*> genExprDynamic env
+  , defer \_ -> Map2 <$> genFn <*> genExprDynamic env <*> genExprDynamic env
+  ]
 
-genExprValue :: Gen Expr_Value
-genExprValue = oneOfRec
-  [ ConstantValue <$> genValue ]
-  [ defer \_ -> ApplyValue <$> genExprValue <*> genExprValue ]
+ppExprDynamic :: Expr_Dynamic -> String
+ppExprDynamic =
+  case _ of
+    Var n -> "v" <> show n
+    Pure v -> "pure " <> v
+    Map f d -> "map " <> f <> " (" <> ppExprDynamic d <> ")"
+    Map2 f d1 d2 -> "map2 " <> f <> " (" <> ppExprDynamic d1 <> ") (" <> ppExprDynamic d2 <> ")"
