772aad1dc4040dcf8c896ad0ae822adadbac23f7
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2020-11-27 11:48:18 +0100

    lumiform - example of traversing in forms

--- test/Specular/Lumiform/LumiformSpec.purs
+++ test/Specular/Lumiform/LumiformSpec.purs
@@ -5,6 +5,7 @@ import Prelude
 import Data.Either (Either(..))
 import Data.Maybe (maybe)
 import Data.String (length, null)
+import Data.Traversable (for, intercalate)
 import Effect (Effect)
 import Specular.Dom.Element (dynText)
 import Specular.Dom.Widget (runMainWidgetInBody)
@@ -12,7 +13,8 @@ import Specular.Lumiform.Lumiform (textInput, form, section)
 
 data Person = Person {
   firstName ::String,
-  lastName :: String
+  lastName :: String,
+  addressLines :: Array String
 }
 
 main :: Effect Unit
@@ -23,9 +25,10 @@ main = do
       section "Person data"
       firstName <- textInput "First name" (\s -> if null s then Left "First name must not be empty" else Right s)
       lastName <- textInput "Last name" (\s -> if length s < 3 then Left "Last name must be at least 3 characters" else Right s)
-      in Person { firstName, lastName }
+      addressLines <- for [1, 2, 3] $ \addressLineNumber -> textInput ("Address line " <> show addressLineNumber) Right
+      in Person { firstName, lastName, addressLines }
     -- here we continue regular usage of Specular
     dynText $ ("Hello, " <> _) <<< maybe "uknown" showPerson <$> person
     where
       showPerson :: Person -> String
-      showPerson (Person r) = r.firstName <> " " <> r.lastName
+      showPerson (Person r) = r.firstName <> " " <> r.lastName <> ", " <> intercalate " " r.addressLines
