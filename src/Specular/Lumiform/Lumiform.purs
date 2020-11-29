ca63eb60316d223622a43075ba734c7037e7b55c
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2020-11-29 22:37:31 +0100

    lumiform POC

--- src/Specular/Lumiform/Lumiform.purs
+++ src/Specular/Lumiform/Lumiform.purs
@@ -1,11 +1,16 @@
-module Specular.Lumiform.Lumiform where
+module Specular.Form.Form where
 
 import Prelude
 
 import Control.Applicative.Free (FreeAp, foldFreeAp, liftFreeAp)
-import Data.Either (Either, either)
+import Data.Array (uncons, mapMaybe)
+import Data.Either (Either(..), either)
 import Data.Functor.Contravariant ((>$<))
-import Data.Maybe (Maybe(..))
+import Data.Int as Int
+import Data.Maybe (Maybe(..), maybe)
+import Data.NonEmpty (NonEmpty(..), head)
+import Data.Number as Num
+import Data.String.CodeUnits (toChar)
 import Data.Tuple (Tuple(..))
 import Specular.Callback (attachEvent)
 import Specular.Dom.Builder.Class (domEventWithSample, elDynAttr')
@@ -15,70 +20,153 @@ import Specular.Dom.Widgets.Input (getTextInputValue, setTextInputValue)
 import Specular.FRP (Dynamic, subscribeDyn_)
 import Specular.Ref (Ref(..), new)
 
-textInputWidget :: (String -> Either String String) -> Widget (Dynamic (Maybe String))
-textInputWidget validation = do
+-- DSL
+type Error = String
+type Constraint a = (a -> Maybe Error)
+type Constraints a = Array (Constraint a)
+
+assert :: forall a . (a -> Boolean) -> Error -> Constraint a
+assert ass error a = if not (ass a) then Just error else Nothing
+
+cmapConstraints :: forall a b . (b -> a) -> Constraints a -> Constraints b
+cmapConstraints f cs = (\c -> c <<< f) <$> cs
+cMaybeMapConstraints :: forall a b . (b -> Maybe a) -> Error -> Constraints a -> Constraints b
+cMaybeMapConstraints f err = map $ \ca b -> case f b of
+  Nothing -> Just err
+  Just a -> ca a
+
+data FormF a =
+  -- metadata
+  Section String a
+  -- base type inputs
+  | StringInput String (Constraints String) (String -> a)
+  | BooleanInput String (Constraints Boolean) (Boolean -> a)
+  | IntInput String (Constraints Int) (Int -> a)
+  | NumberInput String (Constraints Number) (Number -> a)
+  | CharInput String (Constraints Char) (Char -> a)
+  -- BoundedEnum a => Show a => Read a
+  | EnumSelect String (Array String) (Constraints String) (String -> a)
+  | EnumMultiSelect String (Array String) (Constraints (Array String)) (Array String -> a)
+
+instance functorLumiformF :: Functor FormF where
+  map f (Section s a) = Section s (f a)
+  map f (StringInput l v g) = StringInput l v (f <<< g)
+  map f (BooleanInput l v g) = BooleanInput l v (f <<< g)
+  map f (IntInput l v g) = IntInput l v (f <<< g)
+  map f (NumberInput l v g) = NumberInput l v (f <<< g)
+  map f (CharInput l v g) = CharInput l v (f <<< g)
+  map f (EnumSelect l o v g) = EnumSelect l o v (f <<< g)
+  map f (EnumMultiSelect l o v g) = EnumMultiSelect l o v (f <<< g)
+
+type Form = FreeAp FormF
+
+section :: String -> Form Unit
+section s = liftFreeAp $ Section s unit
+
+string :: String -> (Constraints String) -> Form String
+string label constraints = liftFreeAp $ StringInput label constraints identity
+
+boolean :: String -> (Constraints Boolean) -> Form Boolean
+boolean label constraints = liftFreeAp $ BooleanInput label constraints identity
+
+int :: String -> (Constraints Int) -> Form Int
+int label constraints = liftFreeAp $ IntInput label constraints identity
+
+number :: String -> (Constraints Number) -> Form Number
+number label constraints = liftFreeAp $ NumberInput label constraints identity
+
+char :: String -> (Constraints Char) -> Form Char
+char label constraints = liftFreeAp $ CharInput label constraints identity
+
+enumSelect :: String -> Array String -> (Constraints String) -> Form String
+enumSelect label options constraints = liftFreeAp $ EnumSelect label options constraints identity
+
+-- TODO
+-- enumSelect :: forall a . BoundedEnum a => Show a => Read a => String -> Array a -> (Constraints a) -> Form a
+-- enumSelect label options constraints = liftFreeAp $ EnumSelect label (show <$> options) (cmapConstraints (fromMaybe (unsafeThrow "!") <<< read) constraints) identity
+
+-- interpreter
+data WebForm a = WebForm (Widget (Dynamic (Maybe a)))
+
+unform :: forall a . WebForm a -> Widget (Dynamic (Maybe a))
+unform (WebForm w) = w
+
+instance formFunctor :: Functor WebForm where
+  map f (WebForm w)= WebForm (map (map (map f)) w)
+
+instance formApplicative :: Applicative WebForm where
+  pure = WebForm <<< pure <<< pure <<< pure
+
+instance formApply :: Apply WebForm where
+  apply (WebForm wdf) (WebForm wda) = WebForm $ (\dmf dma -> (<*>) <$> dmf <*> dma) <$> wdf <*> wda
+
+webform' :: forall a . Form a -> WebForm a
+webform' = foldFreeAp go
+  where
+    go :: forall a . FormF a -> WebForm a
+    go (Section s a) = WebForm $ do
+      el "h1" [] $ text s
+      pure $ pure $ pure a
+    go (StringInput label constraints f) = WebForm $ do
+      el "div" [] do
+        text label
+        el "div" [] do
+          dyn <- stringInputWidget constraints
+          pure (map f <$> dyn)
+    go (IntInput label constraints f) = WebForm $ do
+      el "div" [] do
+        text label
+        el "div" [] do
+          dyn <- intInputWidget constraints
+          pure (map f <$> dyn)
+    go (NumberInput label constraints f) = WebForm $ do
+      el "div" [] do
+        text label
+        el "div" [] do
+          dyn <- numberInputWidget constraints
+          pure (map f <$> dyn)
+    go (CharInput label constraints  f) = WebForm $ do
+      el "div" [] do
+        text label
+        el "div" [] do
+          dyn <- charInputWidget constraints
+          pure (map f <$> dyn)
+    go (BooleanInput _ _ (f :: Boolean -> a)) = WebForm $ do
+      dyn <- pure $ pure $ pure true
+      pure (map f <$> dyn)
+    go (EnumSelect _ _ _ f) = WebForm $ do
+      dyn <- pure $ pure $ pure "a"
+      pure (map f <$> dyn)
+    go (EnumMultiSelect _ _ _ f) = WebForm $ do
+      dyn <- pure $ pure $ pure ["a", "b", "c"]
+      pure (map f <$> dyn)
+
+stringInputWidget :: Constraints String -> Widget (Dynamic (Maybe String))
+stringInputWidget constraints = do
   ref@(Ref dyn updateRef) <- new ""
   Tuple element _ <- elDynAttr' "input" (pure mempty) (pure unit)
   Tuple velement _ <- elDynAttr' "span" (pure mempty) (pure unit)
   subscribeDyn_ (setTextInputValue element) dyn
   domChanged <- domEventWithSample (\_ -> getTextInputValue element) "input" element
   attachEvent domChanged (const >$< updateRef)
-  dynText $ either identity (const "") <<< validation <$> dyn
-  pure $ (\s -> either (const Nothing) (const (Just s)) (validation s)) <$> dyn
+  dynText $ either head (const "") <<< valid constraints <$> dyn
+  pure $ either (const Nothing) Just <<< valid constraints <$> dyn
+    where
+    valid :: forall a . Constraints a -> a -> Either (NonEmpty Array Error) a
+    valid cs a = maybe (Right a) Left $ toNonEmpty $ mapMaybe (\c -> c a) cs
+      where
+      toNonEmpty :: forall a . Array a -> Maybe (NonEmpty Array a)
+      toNonEmpty = map (\{ head, tail} -> NonEmpty head tail) <<< uncons
 
--- DSL
-data LumiformF a =
-    Output String a
-  | Section String a
-  | TextInput String (String -> Either String String) (String -> a)
+intInputWidget :: Constraints Int -> Widget (Dynamic (Maybe Int))
+intInputWidget constraints = map (map (_ >>= Int.fromString)) $ stringInputWidget (cMaybeMapConstraints Int.fromString "must be integer number" constraints)
 
-instance functorLumiformF :: Functor LumiformF where
-  map f (Output s a) = Output s (f a)
-  map f (Section s a) = Section s (f a)
-  map f (TextInput l v g) = TextInput l v (f <<< g)
+numberInputWidget :: Constraints Number -> Widget (Dynamic (Maybe Number))
+numberInputWidget constraints = map (map (_ >>= Num.fromString)) $ stringInputWidget (cMaybeMapConstraints Num.fromString "must be number" constraints)
 
-type Lumiform = FreeAp LumiformF
+charInputWidget :: Constraints Char -> Widget (Dynamic (Maybe Char))
+charInputWidget constraints = map (map (_ >>= toChar)) $ stringInputWidget (cMaybeMapConstraints toChar "must be a character" constraints)
 
-output :: String -> Lumiform Unit
-output str = liftFreeAp $ Output str unit
 
-section :: String -> Lumiform Unit
-section s = liftFreeAp $ Section s unit
-
-textInput :: String -> (String -> Either String String) -> Lumiform String
-textInput label validation = liftFreeAp $ TextInput label validation identity
-
--- interpreter
-data Form a = Form (Widget (Dynamic (Maybe a)))
-
-unform :: forall a . Form a -> Widget (Dynamic (Maybe a))
-unform (Form w) = w
-
-instance formFunctor :: Functor Form where
-  map f (Form w)= Form (map (map (map f)) w)
-
-instance formApplicative :: Applicative Form where
-  pure = Form <<< pure <<< pure <<< pure
-
-instance formApply :: Apply Form where
-  apply (Form wdf) (Form wda) = Form $ (\dmf dma -> (<*>) <$> dmf <*> dma) <$> wdf <*> wda
-
-lumiform' :: forall a . Lumiform a -> Form a
-lumiform' = foldFreeAp go
-  where
-    go :: forall a . LumiformF a -> Form a
-    go (Output str a) = Form $ do
-      el "div" [] $ text str
-      pure (pure (pure a))
-    go (TextInput label (validate :: String -> Either String String) (f :: String -> a)) = Form $ do
-      el "div" [] do
-        text label
-        el "div" [] do
-          dyn <- textInputWidget validate
-          pure (map f <$> dyn)
-    go (Section s a) = Form $ do
-      el "h1" [] $ text s
-      pure $ pure $ pure a
-
-form :: forall a . Lumiform a -> Widget (Dynamic (Maybe a))
-form = unform <<< lumiform'
+webform :: forall a . Form a -> Widget (Dynamic (Maybe a))
+webform = unform <<< webform'
--- test/Specular/Lumiform/LumiformSpec.purs
+++ test/Specular/Lumiform/LumiformSpec.purs
@@ -2,31 +2,49 @@ module Specular.Lumiform.LumiformSpec where
 
 import Prelude
 
-import Data.Either (Either(..))
+import Data.Char.Unicode (isLetter)
 import Data.Maybe (maybe)
 import Data.String (length, null)
 import Data.Traversable (for, intercalate)
 import Effect (Effect)
 import Specular.Dom.Element (dynText)
 import Specular.Dom.Widget (runMainWidgetInBody)
-import Specular.Lumiform.Lumiform (textInput, form, section)
+import Specular.Lumiform.Lumiform (assert, char, int, number, section, string, webform)
+
+data Status = Enabled | Disabled
 
 data Person = Person {
   firstName ::String,
   lastName :: String,
-  addressLines :: Array String
+  middleInitial :: Char,
+  addressLines :: Array String,
+  age :: Int,
+  height :: Number
 }
-
+-- TODO:
+-- make WebForm a MonadRec
+-- Form and WebForm Form interpreter in separate modules (Form should be lightweight)
+-- value options
+-- web form interpreter for Boolean and enums
 main :: Effect Unit
 main = do
   runMainWidgetInBody do
     -- here's an introduction of lumi-like form
-    person <- form ado
+    person <- webform ado
       section "Person data"
-      firstName <- textInput "First name" (\s -> if null s then Left "First name must not be empty" else Right s)
-      lastName <- textInput "Last name" (\s -> if length s < 3 then Left "Last name must be at least 3 characters" else Right s)
-      addressLines <- for [1, 2, 3] $ \addressLineNumber -> textInput ("Address line " <> show addressLineNumber) Right
-      in Person { firstName, lastName, addressLines }
+      firstName <- string "First name" [
+        assert (not <<< null) "must be not empty",
+        assert ((_ < 3) <<< length) "must be shorter than 3 characters"]
+      lastName <- string "Last name" [
+        assert ((_ >= 3) <<< length) "must be at least 3 characters long"]
+      middleInitial <- char "Middle initial" [
+        assert isLetter "must be a letter"]
+      addressLines <- for [1, 2, 3] $ \addressLineNumber -> string ("Address line " <> show addressLineNumber) mempty
+      age <- int "Age" [
+        assert (_ >= 0) "must not be negative"]
+      height <- number "Height" [
+        assert (_ > 0.0) "must be positive"]
+      in Person { firstName, lastName, middleInitial, addressLines, age, height}
     -- here we continue regular usage of Specular
     dynText $ ("Hello, " <> _) <<< maybe "uknown" showPerson <$> person
     where
