b92bbce8f03df8a62a615575cdc7248f5231acdb
Maciej Bielecki <zyla@prati.pl>
2021-02-12 10:06:39 +0100

    Fix window->global

--- src/Specular/Internal/Profiling.js
+++ src/Specular/Internal/Profiling.js
@@ -1,4 +1,4 @@
-const enabled = !!window.SPECULAR_PROFILING_ENABLED;
+const enabled = !!global.SPECULAR_PROFILING_ENABLED;
 
 const frameNameToIndex = {};
 const frames = [];
@@ -22,7 +22,7 @@ function prepareProfile() {
   };
 }
 
-window.SpecularProfiling = {
+global.SpecularProfiling = {
   open: (url) => {
     const w = window.open(url || 'http://localhost:1234');
     window.addEventListener('message', (event) => {
