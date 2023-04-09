c423ed00c4dc29149ca4f8540a8c8818de4ca78e
Maciej Bielecki <zyla@prati.pl>
2023-04-09 05:09:58 +0000

    We don't care about WeakDynamic

--- bench/Bench/Primitives.purs
+++ bench/Bench/Primitives.purs
@@ -1,6 +1,5 @@
 module Bench.Primitives
   ( dynamicTests
-  , weakDynamicTests
   ) where
 
 import Prelude
@@ -13,7 +12,7 @@ import Data.Foldable (for_, sum)
 import Data.Traversable (sequence)
 import Data.Tuple (Tuple(..), fst)
 import Effect (Effect)
-import Specular.FRP (Dynamic, WeakDynamic, holdDyn, holdWeakDyn, never, newDynamic, newEvent, subscribeWeakDyn_)
+import Specular.FRP (Dynamic, holdDyn, newDynamic, newEvent)
 import Specular.FRP.Base (subscribeDyn_)
 import Specular.Ref as Ref
 
@@ -95,37 +94,7 @@ testDynFn1 fn =
     subscribeDyn_ (\_ -> pure unit) dyn'
     pure (event.fire 1)
 
-testDynFn2 :: (Dynamic Int -> Dynamic Int -> Host (Dynamic Int)) -> Effect (Effect Unit)
-testDynFn2 fn =
-  runHost do
-    event <- newEvent
-    dyn <- holdDyn 0 event.event
-    dyn2 <- holdDyn 0 never
-    dyn' <- fn dyn dyn2
-    subscribeDyn_ (\_ -> pure unit) dyn'
-    pure (event.fire 1)
-
 type Host = CleanupT Effect
 
 runHost :: forall a. Host a -> Effect a
 runHost = map fst <<< runCleanupT
-
-weakDynamicTests :: Tests
-weakDynamicTests =
-  [ Tuple "weak dyn" $ testWeakDynFn1 pure
-  , Tuple "weak dyn fmap" $ testWeakDynFn1 \d -> pure (add 1 <$> d)
-  , Tuple "weak dyn ap pure" $ testWeakDynFn1 \d -> pure (pure (const 1) <*> d)
-  , Tuple "weak dyn ap self" $ testWeakDynFn1 \d -> pure (add <$> d <*> d)
-  , Tuple "weak dyn bind self" $ testWeakDynFn1 \d -> pure (d >>= \_ -> d)
-  , Tuple "weak dyn bind inner" $ testWeakDynFn1 \d -> pure (pure 10 >>= \_ -> d)
-  , Tuple "weak dyn bind outer" $ testWeakDynFn1 \d -> pure (d >>= \_ -> pure 10)
-  ]
-
-testWeakDynFn1 :: (WeakDynamic Int -> Host (WeakDynamic Int)) -> Effect (Effect Unit)
-testWeakDynFn1 fn =
-  runHost do
-    event <- newEvent
-    dyn <- holdWeakDyn event.event
-    dyn' <- fn dyn
-    subscribeWeakDyn_ (\_ -> pure unit) dyn'
-    pure ( event.fire 1)
